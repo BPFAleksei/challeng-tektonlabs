f2da96a517185144119031af5507d66ed993d81f
aleksei escobar <aleksei2006@gmail.com>
2025-04-24 21:43:03 -0600

    "feat: Inicialización del proyecto Spring Boot con Docker Compose y configuración de BD"

---
+++ src/main/java/com/tektonlabs/percentadder/PercentadderApplication.java
@@ -0,0 +1,13 @@
+package com.tektonlabs.percentadder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PercentadderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PercentadderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tektonlabs/percentadder/PercentadderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tektonlabs.percentadder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PercentadderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
